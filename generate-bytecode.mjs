5b10256f511482ea2d81def5aeab73eb395629cd
doomsower <12031673+doomsower@users.noreply.github.com>
2024-09-02 14:19:55 -0300

    fix: bytecode generation after adding gho

--- generate-bytecode.mjs
+++ generate-bytecode.mjs
@@ -1,14 +1,18 @@
 import AaveFLTaker from "./forge-out/AaveFLTaker.sol/AaveFLTaker.json" assert { type: "json" };
 import BatchLiquidator from "./forge-out/BatchLiquidator.sol/BatchLiquidator.json" assert { type: "json" };
-import Liquidator from "./forge-out/Liquidator.sol/Liquidator.json" assert { type: "json" };
+import AaveLiquidator from "./forge-out/AaveLiquidator.sol/AaveLiquidator.json" assert { type: "json" };
 import PriceHelper from "./forge-out/PriceHelper.sol/PriceHelper.json" assert { type: "json" };
+import GhoFMTaker from "./forge-out/GhoFMTaker.sol/GhoFMTaker.json" assert { type: "json" };
+import GhoLiquidator from "./forge-out/GhoLiquidator.sol/GhoLiquidator.json" assert { type: "json" };
 import { writeFileSync } from "node:fs";
 
 const address = "`0x${string}`";
 
 const bytecode = `export const AaveFLTaker_bytecode: ${address} = "${AaveFLTaker.bytecode.object}";
+export const AaveLiquidator_bytecode: ${address} = "${AaveLiquidator.bytecode.object}";
 export const BatchLiquidator_bytecode: ${address} = "${BatchLiquidator.bytecode.object}";
-export const Liquidator_bytecode: ${address} = "${Liquidator.bytecode.object}";
+export const GhoFMTaker_bytecode: ${address} = "${GhoFMTaker.bytecode.object}";
+export const GhoLiquidator_bytecode: ${address} = "${GhoLiquidator.bytecode.object}";
 export const PriceHelper_bytecode: ${address} = "${PriceHelper.bytecode.object}";
 `;
 
